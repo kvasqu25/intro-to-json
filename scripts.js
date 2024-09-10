54bea7416ee31b6412b6332aa4ce0135af907f86
Karen Vasquez <111911263+kvasqu25@users.noreply.github.com>
2024-09-10 16:13:18 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,7 +60,7 @@ function displayPhotos(json){
     for(let photo of json){
         // TO DO
         output += `
-            <img src="${"add image url here"}" alt="" width="${"add image width here"}" height="${"add image height here"}">
+            <img src="${photo.url}" alt="" width="${photo.width}" height="${photo.height}">
         `;
 
     }
@@ -71,14 +71,44 @@ function displayPhotos(json){
 // the function that will display the data from our JSON file of users to the console
 function getUsers(data){
     console.log('This is our user data from the file: ', data);
-    // TO DO - ADD USER INFO TO PAGE
+    /*
+        for each user
+        <section>
+        -name (h3)
+        -username (p)
+        -image (large)
+        -birthday (month, day, year)(p)
+        -email address (as a clickable link) (a)
+        <section>
+    -
+    */
+   let outputDiv = document.getElementById("users")
+
+    let birthday;
+    let output = "";
+
+        for(let user of data.results){
+            console.log(user);
+            birthday = new Date(user.dob.date);
+            output += `
+                <Selection>
+                    <h3>${user.name.first} ${user.name.last}$</h3>
+                    <p>Username: ${user.login.username} $ </p>
+                    <img src="${user.picture.large}$" alt="" width="160" height="160">
+                    <p>Birthday: ${birthday.getMonth() + 1} - ${birthday.getFullYear()} </p>
+                    <a href="mailto:${user.email}">${user.email}</a>
+                </Selection>
+            
+                `;
+        }
+    outputDiv.innerHTML = output;
 }
 
 // call the function to display the cat photos on page load
 // the window.onload handler is where we can attach event handlers 
 // that won't be added to the page until the content has loaded
 window.onload = function(){
-    getPhotos();
+    //getPhotos();
 
     // get the data stored in our local JSON file so we can use it 
     // to display portions of the data returned
